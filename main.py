5b00a8594d27af300e88534defff147265b376b2
Cheng-Ying Li <chengying.li@bridgewell.com>
2022-07-18 19:55:42 +0800

    feat: let test more readable

--- main.py
+++ main.py
@@ -16,7 +16,16 @@ class WasRun(TestCase):
         self.wasRun = 1
 
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert (not test.wasRun)
+        test.run()
+        assert (test.wasRun)
+
+
+TestCaseTest("testRunning").run()
+# test = WasRun("testMethod")
+# print(test.wasRun)
+# test.run()
+# print(test.wasRun)
